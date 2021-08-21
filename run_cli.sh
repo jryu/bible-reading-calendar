b7e51116046c017bbd0533530c23c17b2341f01a
Junho Ryu <jryu@users.noreply.github.com>
2021-08-21 02:06:56 -0700

    Build before running commands

--- run_cli.sh
+++ run_cli.sh
@@ -1,1 +1,6 @@
+set -e
+
+cd build
+cmake --build .
+cd ..
 build/cli --bible_reading_plans_path=/home/jryu/bible-reading-calendar/bible-reading-plans/
--- run_cms.sh
+++ run_cms.sh
@@ -1,1 +1,6 @@
+set -e
+
+cd build
+cmake --build .
+cd ..
 build/cms --undefok=c --bible_reading_plans_path=/home/jryu/bible-reading-calendar/bible-reading-plans/ -c cms_config.js
