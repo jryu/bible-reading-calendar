4e2d691a8839f8c2c77df6fbac4c3ca6bd82df0e
Junho Ryu <jryu@users.noreply.github.com>
2021-08-29 21:12:33 -0700

    Remove zip file support

--- src/main_cms.cpp
+++ src/main_cms.cpp
@@ -9,11 +9,6 @@
 #include <iostream>
 #include <time.h>
 
-#include "minizip/mz.h"
-#include "minizip/mz_os.h"
-#include "minizip/mz_strm_mem.h"
-#include "minizip/mz_strm.h"
-#include "minizip/mz_zip.h"
 #include "calendar.h"
 #include "config.pb.h"
 
@@ -25,7 +20,6 @@ class CalendarApp : public cppcms::application {
       dispatcher().assign("/img.svg", &CalendarApp::svg, this);
       dispatcher().assign("/img.pdf", &CalendarApp::pdf, this);
       dispatcher().assign("/img.png", &CalendarApp::png, this);
-      dispatcher().assign("/img.zip", &CalendarApp::zip, this);
       dispatcher().assign(".*", &CalendarApp::redirect, this);
     }
 
@@ -37,10 +31,6 @@ class CalendarApp : public cppcms::application {
     void svg();
     void pdf();
     void png();
-    void zip();
-
-    mz_zip_file buildFileInfo(const char* filename);
-    void streamZipOutput(void* write_mem_stream);
 
     static cairo_status_t cairoWriteFunc(
         void* closure, const unsigned char* data, unsigned int length) {
@@ -64,7 +54,6 @@ class CalendarApp : public cppcms::application {
 
   private:
     config::CalendarConfig buildConfig();
-    void copyFromCairoBufferToZipEntry(void* zip_handle,mz_zip_file file_info);
 
     unsigned char* cairoBuffer_;
     unsigned int cairoBufferOffset_;
@@ -203,121 +192,6 @@ void CalendarApp::png()
 
   Calendar calendar(buildConfig());
   calendar.streamPng(CalendarApp::cairoWriteFunc, this);
-}
-
-void CalendarApp::zip()
-{
-  response().set_header("Content-Type", "application/zip");
-  response().cache_control("public, max-age=3600");
-
-  void *write_mem_stream = NULL;
-  mz_stream_mem_create(&write_mem_stream);
-  mz_stream_mem_set_grow_size(write_mem_stream, 128 * 1024);
-  mz_stream_open(write_mem_stream, NULL, MZ_OPEN_MODE_CREATE);
-
-  void *zip_handle = NULL;
-  mz_zip_create(&zip_handle);
-  int32_t err = mz_zip_open(zip_handle, write_mem_stream, MZ_OPEN_MODE_WRITE);
-  if (err != MZ_OK) {
-    response().status(500);
-    logger->error("mz_zip_open error: {}", err);
-    return;
-  }
-
-  config::CalendarConfig conf = buildConfig();
-  cairoBuffer_ = (unsigned char*) malloc(200 *1024);
-  for (int i = 0; i < 12 ; ++i) {
-    conf.set_month(i + 1);
-    cairoBufferOffset_ = 0;
-
-    Calendar calendar(conf);
-    calendar.streamPdf(CalendarApp::cairoWriteToMemoryFunc, this);
-
-    char filename[100];
-    sprintf(filename, "%02d.pdf", conf.month());
-    mz_zip_file file_info = buildFileInfo(filename);
-    copyFromCairoBufferToZipEntry(zip_handle, file_info);
-  }
-  free(cairoBuffer_);
-
-  mz_zip_close(zip_handle);
-  mz_zip_delete(&zip_handle);
-
-  streamZipOutput(write_mem_stream);
-
-  mz_stream_mem_close(write_mem_stream);
-  mz_stream_mem_delete(&write_mem_stream);
-  write_mem_stream = NULL;
-}
-
-mz_zip_file CalendarApp::buildFileInfo(const char* filename)
-{
-  mz_zip_file file_info;
-  memset(&file_info, 0, sizeof(file_info));
-
-  file_info.version_madeby = MZ_VERSION_MADEBY;
-  file_info.compression_method = MZ_COMPRESS_METHOD_DEFLATE;
-  time(&file_info.modified_date);
-
-  uint32_t src_attrib = S_IFREG | 0664;
-  uint8_t src_sys = MZ_HOST_SYSTEM(file_info.version_madeby);
-
-  if ((src_sys != MZ_HOST_SYSTEM_MSDOS) &&
-      (src_sys != MZ_HOST_SYSTEM_WINDOWS_NTFS)) {
-    // High bytes are OS specific attributes,low byte is always DOS attributes.
-    uint32_t target_attrib = 0;
-    if (mz_zip_attrib_convert(
-          src_sys, src_attrib, MZ_HOST_SYSTEM_MSDOS, &target_attrib) == MZ_OK) {
-      file_info.external_fa = target_attrib;
-    }
-    file_info.external_fa |= (src_attrib << 16);
-  } else {
-    file_info.external_fa = src_attrib;
-  }
-
-  file_info.filename = filename;
-  file_info.uncompressed_size = cairoBufferOffset_;
-#ifdef HAVE_WZAES
-  file_info.aes_version = MZ_AES_VERSION;
-#endif
-
-  return file_info;
-}
-
-void CalendarApp::copyFromCairoBufferToZipEntry(
-    void* zip_handle, mz_zip_file file_info)
-{
-  int32_t err = mz_zip_entry_write_open(
-      zip_handle, &file_info, MZ_COMPRESS_LEVEL_DEFAULT, 0, NULL);
-  if (err != MZ_OK) {
-    response().status(500);
-    logger->error("mz_zip_entry_write_open error: {}", err);
-    return;
-  }
-
-  int32_t written_total = 0;
-  do {
-    int32_t written = mz_zip_entry_write(zip_handle,
-        cairoBuffer_ + written_total,
-        cairoBufferOffset_ - written_total);
-
-    if (written < MZ_OK) {
-      err = written;
-      break;
-    }
-    written_total += written;
-  } while (err == MZ_OK && written_total < cairoBufferOffset_);
-
-  mz_zip_entry_close(zip_handle);
-}
-
-void CalendarApp::streamZipOutput(void* write_mem_stream)
-{
-  const char *buffer_ptr = NULL;
-  mz_stream_mem_get_buffer(write_mem_stream, (const void **)&buffer_ptr);
-  mz_stream_mem_seek(write_mem_stream, 0, MZ_SEEK_END);
-
-  response().out().write(buffer_ptr, mz_stream_mem_tell(write_mem_stream));
 }
 
 int main(int argc,char ** argv)
